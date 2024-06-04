35dc93b7b6d8ac7492daed6cb834d074c1a0537f
AndreNovaisBrito <novaisandre.dev@github.com>
2024-06-03 23:29:57 -0300

    Added documentation for clist.rs

--- src/clist.rs
+++ src/clist.rs
@@ -21,6 +21,7 @@ impl Hasher for MemHasher {
     }
 }
 
+///A builder for `MemHasher`.
 #[derive(Clone, Default)]
 struct BuildMemHasher;
 
@@ -32,6 +33,7 @@ impl BuildHasher for BuildMemHasher {
     }
 }
 
+///A builder for `MemHasher`.
 #[derive(Clone, Default)]
 struct Mem<T> {
     data: Vec<T>,
@@ -39,17 +41,20 @@ struct Mem<T> {
 }
 
 impl<T> Mem<T> {
+    /// Creates a `Mem` from a given data vector.
     fn raw(data: Vec<T>) -> Self {
         Self {
             data,
             available: IndexSet::with_hasher(BuildMemHasher),
         }
     }
-
+    
+    /// Creates a `Mem` with specified capacity.
     fn with_capacity(n: usize) -> Self {
         Mem::raw(Vec::with_capacity(n))
     }
 
+    /// Allocates an item and returns it's address;
     fn intern(&mut self, t: T) -> usize {
         if let Some(addr) = self.available.pop() {
             self.data[addr] = t;
@@ -61,23 +66,28 @@ impl<T> Mem<T> {
         }
     }
 
+    /// Gets a reference to an item by it's address.
     fn get(&self, addr: usize) -> &T {
         &self.data[addr]
     }
 
+    /// Gets a mutable reference to an item by it's address.
     fn get_mut(&mut self, addr: usize) -> &mut T {
         &mut self.data[addr]
     }
-
+    
+    /// Frees and alocated slot.
     fn free(&mut self, addr: usize) {
         self.available.insert(addr);
     }
 
+    /// Checks if a slot if not free.
     fn not_free(&self, addr: &usize) -> bool {
         !self.available.contains(addr)
     }
 }
 
+/// Represents a node in a circular doubly linked list.
 #[derive(Clone, Default)]
 struct Node<T> {
     data: Option<T>,
@@ -86,6 +96,8 @@ struct Node<T> {
 }
 
 impl<T> Node<T> {
+    
+    /// Creates a root node with no data.
     fn root() -> Self {
         Self {
             data: None,
@@ -94,6 +106,7 @@ impl<T> Node<T> {
         }
     }
 
+    /// Initializes a node with data.
     fn init(t: T) -> Self {
         Self {
             data: Some(t),
@@ -103,6 +116,7 @@ impl<T> Node<T> {
     }
 }
 
+/// A circular doubly linked list with custom memory management.
 #[derive(Clone, Default)]
 pub struct CList<T> {
     mem: Mem<Node<T>>,
@@ -112,6 +126,16 @@ pub struct CList<T> {
 pub const ROOT_ADDR: usize = 0;
 
 impl<T> CList<T> {
+    
+    /// Creates a list with specified capacity.
+    ///
+    /// # Arguments
+    ///
+    /// * `n` - The capacity of the list.
+    ///
+    /// # Returns 
+    ///
+    /// A  `CList` with the specified capacity.
     #[inline]
     pub fn with_capacity(n: usize) -> Self {
         let mut mem = Mem::with_capacity(n + 1);
@@ -119,6 +143,15 @@ impl<T> CList<T> {
         Self { mem, len: 0 }
     }
 
+    /// Creates a list from a slice. 
+    ///
+    /// # Arguments
+    ///
+    /// * `slice` - A slice of elements. 
+    ///
+    /// # Returns 
+    ///
+    /// A  `CList` containing the elements of the slice. 
     pub fn from_slice(slice: &[T]) -> Self
     where
         T: Clone,
@@ -140,7 +173,16 @@ impl<T> CList<T> {
         }
     }
 
-    pub fn from_vec(vec: Vec<T>) -> Self
+    /// Creates a list from a vector.
+    ///
+    /// # Arguments
+    ///
+    /// * `vec` - A vector of elements. 
+    ///
+    /// # Returns 
+    ///
+    /// A  `CList` containing the elements of the vector.
+   pub fn from_vec(vec: Vec<T>) -> Self
     where
         T: Clone,
     {
@@ -165,21 +207,45 @@ impl<T> CList<T> {
         }
     }
 
-    #[inline]
+    /// Returns the number of elements in the list.
+    ///
+    /// # Returns 
+    ///
+    /// The number of elements in the list.
+   #[inline]
     pub fn len(&self) -> usize {
         self.len
     }
 
+    /// Checks if the list is empty
+    ///
+    /// # Returns 
+    ///
+    /// `true` if the list is empty, `false` otherwise.
     #[inline]
     pub fn is_empty(&self) -> bool {
         self.len == 0
     }
 
+    /// Asserts that the node at the given address is not free.
+    ///
+    /// # Arguments
+    ///
+    /// * `node_addr` - The address of the node.
     fn assert_not_free(&self, node_addr: &usize) {
         assert!(self.mem.not_free(node_addr), "Node address is free.");
     }
 
-    #[inline]
+    /// Gets a reference to the data at the given node address.
+    ///
+    /// # Arguments
+    ///
+    /// * `node_addr` - the address of the node.
+    ///
+    /// # Returns
+    ///
+    /// A reference to the data at the node address.
+   #[inline]
     pub fn get(&self, node_addr: usize) -> &T {
         self.assert_not_free(&node_addr);
         self.mem
@@ -189,6 +255,15 @@ impl<T> CList<T> {
             .expect("Address doesn't contain data.")
     }
 
+    /// Gets a mutable reference to the data at the given node address.
+    /// 
+    /// # Arguments
+    ///
+    /// * `node_addr` - the address of the node.
+    ///
+    /// # Returns
+    ///
+    /// A mutable reference to the data at the node address.
     #[inline]
     pub fn get_mut(&mut self, node_addr: usize) -> &mut T {
         self.assert_not_free(&node_addr);
@@ -199,29 +274,76 @@ impl<T> CList<T> {
             .expect("Address doesn't contain data.")
     }
 
-    #[inline]
+    /// Gets the previous node address of the given node address.
+    /// 
+    /// # Arguments
+    ///
+    /// * `node_addr` - the address of the node.
+    ///
+    /// # Returns
+    ///
+    /// The previous node address.
+   #[inline]
     pub fn prev(&self, node_addr: usize) -> usize {
         self.assert_not_free(&node_addr);
         self.mem.get(node_addr).prev
     }
 
-    #[inline]
+    /// Gets the next node address of the given node address.
+    /// 
+    /// # Arguments
+    ///
+    /// * `node_addr` - the address of the node.
+    ///
+    /// # Returns
+    ///
+    /// The next node address.
+   #[inline]
     pub fn next(&self, node_addr: usize) -> usize {
         self.assert_not_free(&node_addr);
         self.mem.get(node_addr).next
     }
 
-    #[inline]
+    /// Gets the address of the first node.
+    /// 
+    /// # Arguments
+    ///
+    /// * `node_addr` - the address of the node.
+    ///
+    /// # Returns
+    ///
+    /// The address of the first node.
+   #[inline]
     pub fn first_addr(&self) -> usize {
         self.next(ROOT_ADDR)
     }
 
-    #[inline]
+    /// Gets the address of the last node.
+    /// 
+    /// # Arguments
+    ///
+    /// * `node_addr` - the address of the node.
+    ///
+    /// # Returns
+    ///
+    /// The address of the last node.
+   #[inline]
     pub fn last_addr(&self) -> usize {
         self.prev(ROOT_ADDR)
     }
 
-    pub fn insert_ref(&mut self, t: T, ref_node_addr: usize, after: bool) -> usize {
+    /// Inserts a new node with the given data before or after a reference node.
+    /// 
+    /// # Arguments
+    ///
+    /// * `t` - the data to insert.
+    /// * `ref_node_addr` - the address to the reference node.
+    /// * `after` - Whether to insert after (`true`) or before (`false`) the reference node.
+    ///
+    /// # Returns
+    ///
+    /// The address of the new node.
+   pub fn insert_ref(&mut self, t: T, ref_node_addr: usize, after: bool) -> usize {
         self.assert_not_free(&ref_node_addr);
         let new_node_addr = self.mem.intern(Node::init(t));
         let ref_node = self.mem.get_mut(ref_node_addr);
@@ -247,7 +369,16 @@ impl<T> CList<T> {
         new_node_addr
     }
 
-    pub fn remove(&mut self, node_addr: usize) -> usize {
+    /// Removes a node at the given address.
+    /// 
+    /// # Arguments
+    ///
+    /// * `node_addr` - the address of the node to be removed.
+    ///
+    /// # Returns
+    ///
+    /// The address of the next node.
+  pub fn remove(&mut self, node_addr: usize) -> usize {
         if node_addr == ROOT_ADDR {
             return node_addr;
         }
@@ -261,7 +392,17 @@ impl<T> CList<T> {
         right
     }
 
-    pub fn remove_n(&mut self, mut node_addr: usize, n: usize) -> usize {
+    /// Removes at max `n` nodes starting from the given address.
+    /// 
+    /// # Arguments
+    ///
+    /// * `node_addr` - the starting address to the node to remove.
+    /// * `n` - the number of nodes to remove.
+    ///
+    /// # Returns
+    ///
+    /// The address of the node after the last removed node.
+   pub fn remove_n(&mut self, mut node_addr: usize, n: usize) -> usize {
         self.assert_not_free(&node_addr);
         let left = self.mem.get(node_addr).prev;
         let mut removed = 0;
@@ -279,7 +420,17 @@ impl<T> CList<T> {
         node_addr
     }
 
-    #[inline]
+    /// Inserts multiple nodes to the right of the reference node.
+    /// 
+    /// # Arguments
+    ///
+    /// * `ref_node_addr` - The address of the reference node.
+    /// * `data` - An iterator of elements to insert.
+    ///
+    /// # Returns
+    ///
+    /// The address of the last inserted node.
+   #[inline]
     pub fn insert_iter_right<I: IntoIterator<Item = T>>(
         &mut self,
         ref_node_addr: usize,
@@ -290,7 +441,17 @@ impl<T> CList<T> {
         })
     }
 
-    #[inline]
+    /// Inserts multiple nodes to the left of the reference node.
+    /// 
+    /// # Arguments
+    ///
+    /// * `ref_node_addr` - The address of the reference node.
+    /// * `data` - An iterator of elements to insert.
+    ///
+    /// # Returns
+    ///
+    /// The address of the last inserted node.
+   #[inline]
     pub fn insert_iter_left<I: IntoIterator<Item = T>>(
         &mut self,
         ref_node_addr: usize,
@@ -304,7 +465,18 @@ impl<T> CList<T> {
         })
     }
 
-    fn populate_buf(&self, mut buf: Vec<T>, mut node_addr: usize) -> Vec<T>
+    /// Fills a buffer with the data from the list starting from a given node address.
+    /// 
+    /// # Arguments
+    ///
+    /// * `buf` - The buffer to fill.
+    /// * `node_addr` - The starting address of the node.
+    /// * `data` - An iterator of elements to insert.
+    ///
+    /// # Returns
+    ///
+    /// The filled buffer.
+   fn populate_buf(&self, mut buf: Vec<T>, mut node_addr: usize) -> Vec<T>
     where
         T: Clone,
     {
@@ -321,7 +493,12 @@ impl<T> CList<T> {
         buf
     }
 
-    #[inline]
+    /// Collects all elements from the list into a vector.
+    ///
+    /// # Returns
+    ///
+    /// A vector containing all the elements in the list.
+   #[inline]
     pub fn collect(&self) -> Vec<T>
     where
         T: Clone,
@@ -332,7 +509,16 @@ impl<T> CList<T> {
         self.populate_buf(Vec::with_capacity(self.len), ROOT_ADDR)
     }
 
-    #[inline]
+    /// Collects all elements from the list, starting from a given address into a vector
+    ///
+    /// # Arguments
+    ///
+    /// * `node_addr` - The address of the starting node.
+    /// 
+    /// # Returns
+    ///
+    /// A vector containing all the elements from the starting address.
+   #[inline]
     pub fn collect_from_addr(&self, node_addr: usize, capacity_hint: Option<usize>) -> Vec<T>
     where
         T: Clone,
@@ -345,7 +531,19 @@ impl<T> CList<T> {
         self.populate_buf(Vec::with_capacity(capacity), node_addr)
     }
 
-    fn populate_buf_n(&self, mut buf: Vec<T>, mut node_addr: usize, mut n: usize) -> Vec<T>
+    /// Fills a buffer with the data from the list starting from a given node address.
+    /// 
+    /// # Arguments
+    ///
+    /// * `buf` - The buffer to fill.
+    /// * `node_addr` - The starting address of the node.
+    /// * `data` - An iterator of elements to insert.
+    /// * `n` - Max number of elements to insert. 
+    ///
+    /// # Returns
+    ///
+    /// The filled buffer.
+   fn populate_buf_n(&self, mut buf: Vec<T>, mut node_addr: usize, mut n: usize) -> Vec<T>
     where
         T: Clone,
     {
@@ -363,7 +561,16 @@ impl<T> CList<T> {
         buf
     }
 
-    #[inline]
+    /// Collects n elements from the list into a vector.
+    /// 
+    /// # Arguments
+    ///
+    /// * `n` - Max number of elements to collect.
+    ///
+    /// # Returns
+    ///
+    /// A vector containing all the elements in the list.
+   #[inline]
     pub fn collect_n(&self, n: usize) -> Vec<T>
     where
         T: Clone,
@@ -374,7 +581,24 @@ impl<T> CList<T> {
         self.populate_buf_n(Vec::with_capacity(n.min(self.len)), ROOT_ADDR, n)
     }
 
-    pub fn collect_n_from_pos(&self, n: usize, pos: usize) -> Vec<T>
+    /// Collects up to `n` elements, starting from a given position in the list
+    ///
+    /// If the list is empty, if `n` is zero, or if the position is out of bounds,
+    /// it returns an empty vector.
+    ///
+    /// # Arguments
+    ///
+    /// * `n` - The the number of elements to collect.
+    /// * `pos` - The starting position in the list.
+    /// 
+    /// # Returns
+    ///
+    /// A vector containing the collected elements.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if the list is modified during iteration.
+   pub fn collect_n_from_pos(&self, n: usize, pos: usize) -> Vec<T>
     where
         T: Clone,
     {
